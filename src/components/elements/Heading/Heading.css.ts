256d14117b1d57d37a4a2793838eafea70cf73bc
r-happy <hr325913@gmail.com>
2024-03-08 21:42:42 +0900

    disable font

--- src/components/elements/Heading/Heading.css.ts
+++ src/components/elements/Heading/Heading.css.ts
@@ -1,9 +1,9 @@
 import { style } from "@vanilla-extract/css";
 import { Colors } from "@/styles/color";
 
-import { semiFont } from "@/assets/font/font";
+// import { semiFont } from "@/assets/font/font";
 
 export const main_style = style({
     borderBottom: `solid 1px ${Colors.border.sub}`,
-    fontFamily: semiFont.style.fontFamily,
+    // fontFamily: semiFont.style.fontFamily,
 });
